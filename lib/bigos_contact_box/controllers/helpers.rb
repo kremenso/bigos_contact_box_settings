cc6967babcc0e807a3ec1de2543905da430c1da6
kremenso <kremenso@o2.pl>
2013-01-29 21:15:01 +0100

    rename settings, add to InstalledModule

--- lib/bigos_contact_box/controllers/helpers.rb
+++ lib/bigos_contact_box/controllers/helpers.rb
@@ -10,16 +10,16 @@ module BigosContactBox
           ActionMailer::Base.raise_delivery_errors = true
           ActionMailer::Base.perform_deliveries = true
           ActionMailer::Base.default :charset => "utf-8"
-          ActionMailer::Base.default_url_options = { :host => Setting["#{BigosContactBox.name}_domain"] }
+          ActionMailer::Base.default_url_options = { :host => Setting["#{BigosContactBox.name}.domain"] }
 
           ActionMailer::Base.smtp_settings = {
-            :address              => Setting["#{BigosContactBox.name}_address_name"].to_s,
-            :port                 => Setting["#{BigosContactBox.name}_port"].to_i,
-            :user_name            => Setting["#{BigosContactBox.name}_user_name"].to_s,
-            :password             => Setting["#{BigosContactBox.name}_password"].to_s,
+            :address              => Setting["#{BigosContactBox.name}.address_name"].to_s,
+            :port                 => Setting["#{BigosContactBox.name}.port"].to_i,
+            :user_name            => Setting["#{BigosContactBox.name}.user_name"].to_s,
+            :password             => Setting["#{BigosContactBox.name}.password"].to_s,
             :authentication       => "plain",
             :enable_starttls_auto => true,
-            :domain               => Setting["#{BigosContactBox.name}_domain"].to_s
+            :domain               => Setting["#{BigosContactBox.name}.domain"].to_s
           }
         end
         @message = BigosContactBox::Message.new
--- lib/generators/bigos_contact_box/install_generator.rb
+++ lib/generators/bigos_contact_box/install_generator.rb
@@ -19,21 +19,21 @@ module BigosContactBox
       end
 
       def create_settings
-        Setting["#{BigosContactBox.name}_use_widget_mailer_settings"] = false
-        Setting["#{BigosContactBox.name}_use_widget_mailer_settings"] = false
-        Setting["#{BigosContactBox.name}_address_name"] = "smtp.gmail.com"
-        Setting["#{BigosContactBox.name}_port"] = 587
-        Setting["#{BigosContactBox.name}_Setting.domain"] = "yourdomain.dev"
-        Setting["#{BigosContactBox.name}_"] = "from@yourdomain.dev"
-        Setting["#{BigosContactBox.name}_password"] = "Super-Secure-Password"
-        Setting["#{BigosContactBox.name}_latitude"] = ""
-        Setting["#{BigosContactBox.name}_longitude"] = ""
-        Setting["#{BigosContactBox.name}_invalid_fields_error"] = "Invalid Fields"
-        Setting["#{BigosContactBox.name}_success_message"] = "Message sent successfully"
-        Setting["#{BigosContactBox.name}_location_name"] = "Location name"
-        Setting["#{BigosContactBox.name}_redirect_path"] = ""
-        Setting["#{BigosContactBox.name}_map_zoom"] = ""
-        Setting["#{BigosContactBox.name}_marker_icon_src"] = ""
+        Setting["#{BigosContactBox.name}.use_widget_mailer_settings"] = false
+        Setting["#{BigosContactBox.name}.address_name"] = "smtp.gmail.com"
+        Setting["#{BigosContactBox.name}.port"] = 587
+        Setting["#{BigosContactBox.name}.domain"] = "yourdomain.dev"
+        Setting["#{BigosContactBox.name}.user_name"] = "from@yourdomain.dev"
+        Setting["#{BigosContactBox.name}.password"] = "Super-Secure-Password"
+        Setting["#{BigosContactBox.name}.latitude"] = ""
+        Setting["#{BigosContactBox.name}.longitude"] = ""
+        Setting["#{BigosContactBox.name}.invalid_fields_error"] = "Invalid Fields"
+        Setting["#{BigosContactBox.name}.success_message"] = "Message sent successfully"
+        Setting["#{BigosContactBox.name}.location_name"] = "Location name"
+        Setting["#{BigosContactBox.name}.redirect_path"] = ""
+        Setting["#{BigosContactBox.name}.map_zoom"] = ""
+        Setting["#{BigosContactBox.name}.marker_icon_src"] = ""
+        BigosApp::InstalledModule.build(name: BigosContactBox.name, page_element: BigosApp::BigosContactBoxEtactlement.name)
       end
 
     end
